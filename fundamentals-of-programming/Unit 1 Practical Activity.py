087b30f1955c58daa945461b2b4cb856c202867c
Anna Buczak <ab25014@essex.ac.uk>
2026-03-27 10:12:54 +0000

    update 1

---
+++ fundamentals-of-programming/Unit 1 Practical Activity.py
@@ -0,0 +1,11 @@
+#Print "Hello, World!".
+print("Hello, World")
+
+#Ask user for two numbers
+#input () gives text (string), we conver it to a number by using float
+num1 = float(input("Enter first number"))
+num2 = float(input("Enter second number"))
+print("Addition",num1 + num2)
+print("Subtraction",num1 - num2)
+print("Multiplication",num1 * num2)
+print("Division",num1 / num2)
