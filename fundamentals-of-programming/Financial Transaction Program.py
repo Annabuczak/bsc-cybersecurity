bb7bbc2d41e76519afcc879e6b6339d7766bae67
Anna Buczak <ab25014@essex.ac.uk>
2026-04-01 13:14:13 +0100

    update 1

--- fundamentals-of-programming/Financial Transaction Program.py
+++ fundamentals-of-programming/Financial Transaction Program.py
@@ -1,36 +1,50 @@
-#Financial Transaction Program
-# Display the main menu options to the user
 transactions = []
+
 def show_menu():
- print("\n--Financial Transaction Program--")
- print("1. Add income")
- print("2. Add expense")
- print("3. View transactions")
- print("4. View balance")
+    print("\n--Financial Transaction Program--")
+    print("1. Add income")
+    print("2. Add expense")
+    print("3. View transactions")
+    print("4. View balance")
+def add_transaction(t_type, t_amount, t_date, t_category):
+    transactions.append({
+        "type": t_type,
+        "amount": t_amount,
+        "date": t_date,
+        "category": t_category
+    })
+    print(f"Success: ${t_amount} added to {t_category} on {t_date}.")
+
+
 while True:
     show_menu()
-    choice = input("Enter your choice: ").strip()
+
+    choice = input("Enter your choice: ")
+
     if choice == "1":
-     print("1.Salary")
-     print("2.Freelance")
-     print("3.Other")
-    income_choice = input("Enter your choice: ")
+        print("1. Salary")
+        print("2. Freelance")
+        print("3. Other")
 
-    if income_choice == "1":
-       amount = input("Enter amount")
-       print("Salary added" , amount)
+        income_choice = input("Enter your choice: ")
 
-    elif income_choice == "2":
-        amount = input("Enter amount")
-        print("Freelance added" , amount)
+        if income_choice == "1":
+            amount = float(input("Enter amount: "))
+            date = input("Enter date (YYYY-MM-DD): ")
+            add_transaction("income", amount, date, "Salary")
 
-    elif income_choice  == "3":
-         amount = input("Enter amount")
-         print("Other" , "amount")
+        elif income_choice == "2":
+            amount = float(input("Enter amount: "))
+            date = input("Enter date (YYYY-MM-DD): ")
+            add_transaction("income", amount, date, "Freelance")
 
-    elif income_choice == "4":
-        print("Exiting...")
-
+        elif income_choice == "3":
+            amount = float(input("Enter amount: "))
+            date = input("Enter date (YYYY-MM-DD): ")
+            add_transaction("income", amount, date, "Other")
+        else:
+            print("Invalid income option")
     else:
         print("Invalid input. Please choose 1-4.")
+
 
