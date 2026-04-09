17578264258585af453a66e70f538682b092c184
Anna Buczak <ab25014@essex.ac.uk>
2026-04-09 10:08:04 +0100

    update 2

--- fundamentals-of-programming/Calculator with Abstraction.py
+++ fundamentals-of-programming/Calculator with Abstraction.py
@@ -8,30 +8,53 @@ class Calculator:
     def multiply(self, a, b):
         return a * b
     def divide(self, a, b):
-     if b ==0:
-       return "Error: Cannot divide by zero"
-     return a / b
+        if b == 0:
+            return "Error: Cannot divide by zero"
+        return a / b
+
 
 def main():
     print("\nCalculator")
-while True:
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
-    print("5. Go to main menu")
-    if __name__ == "__main__":
+    while True:
+         print("1. Add")
+         print("2. Subtract")
+         print("3. Multiply")
+         print("4. Divide")
+         print("5. Exit")
+
+
+
+         print("\nSelect an operation:")
+         choice = input("Enter your choice: ")
+         calc = Calculator()
+
+         if choice == "1":
+             a = float(input("Enter first amount: "))
+             b = float(input("Enter second amount: "))
+             result = calc.add(a, b)
+             print(result)
+         elif choice == "2":
+             a = float(input("Enter first amount:"))
+             b = float(input("Enter second amount: "))
+             result = calc.subtract(a, b)
+             print(result)
+         elif choice == "3":
+             a = float(input("Enter first amount: "))
+             b = float(input("Enter second amount: "))
+             result = calc.multiply(a, b)
+             print(result)
+         elif choice == "4":
+             a = float(input("Enter first amount: "))
+             b = float(input("Enter second amount: "))
+             result = calc.divide(a, b)
+             print(result)
+
+         elif choice == "5":
+              break
+
+         else:
+            print("Exit")
+
+
+if __name__ == "__main__":
         main()
-        print("\nSelect an operation:")
-        choice = input("Enter your choice: ")
-        calc = Calculator()
-        if choice == "1":
-         a = float(input("Enter first amount: "))
-         b = float(input("Enter second amount: "))
-        result = calc.add(a, b)
-        print(result)
-    elif choice == "2":
-     a = float(input("Enter first amount:"))
-    b = float(input("Enter second amount: "))
-    result = calc.subtract(a,b)
-    print(result)
