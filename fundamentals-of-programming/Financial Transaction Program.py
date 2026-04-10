f0ca7162818a7985e630240841e8bb6b74651fcf
Anna Buczak <ab25014@essex.ac.uk>
2026-04-10 10:28:56 +0100

    update 2

--- fundamentals-of-programming/Financial Transaction Program.py
+++ fundamentals-of-programming/Financial Transaction Program.py
@@ -36,81 +36,30 @@ while True:
     menu()
 
     choice = input("Enter your choice: ")
+    while True:
+        if choice == "1":
+            print("1. Salary")
+            print("2. Freelance")
+            print("3. Other")
+            print("4. Exit to main menu")
 
-    if choice == "1":
-        print("1. Salary")
-        print("2. Freelance")
-        print("3. Other")
+            income_choice = input("Enter your choice: ")
 
-        income_choice = input("Enter your choice: ")
+            if income_choice == "1":
+                amount = float(input("Enter amount: "))
+                date = input("Enter date (dd/mm/yyyy): ")
+                add_transactions("income", amount, date, "Salary")
 
-        if income_choice == "1":
-            amount = float(input("Enter amount: "))
-            date = input("Enter date (dd/mm/yyyy): ")
-            add_transactions("income", amount, date, "Salary")
+            elif income_choice == "2":
+                amount = float(input("Enter amount: "))
+                date = input("Enter date (dd/mm/yyyy): ")
+                add_transactions("income", amount, date, "Freelance")
 
-        elif income_choice == "2":
-            amount = float(input("Enter amount: "))
-            date = input("Enter date (dd/mm/yyyy): ")
-            add_transactions("income", amount, date, "Freelance")
+            elif income_choice == "3":
+                amount = float(input("Enter amount: "))
+                date = input("Enter date (dd/mm/yyyy): ")
+                add_transactions("income", amount, date, "Other")
 
-        elif income_choice == "3":
-            amount = float(input("Enter amount: "))
-            date = input("Enter date (dd/mm/yyyy): ")
-            add_transactions("income", amount, date, "Other")
-
-        else:
-            print("Invalid income option.")
-            if not transactions:
-                print("No transactions yet.")
-
-    # expenses
-    elif choice == "2":
-        print("1. Bills")
-        print("2. Groceries")
-        print("3. Transport")
-        print("4. Entertainment")
-        print("5. Other")
-        print("6. Unexpected")
-
-    expense_choice = input("Enter your choice: ")
-
-    if expense_choice == "1":
-        amount = float(input("Enter amount: "))
-        date = input("Enter date (YYYY-MM-DD): ")
-        add_transactions("expense", amount, date, "Bills")
-
-
-    elif expense_choice == "2":
-        amount = float(input("Enter amount: "))
-        date = input("Enter date (dd/mm/yyyy): ")
-        add_transactions("expense", amount, date, "Groceries")
-    elif expense_choice == "3":
-        amount = float(input("Enter amount: "))
-        date = input("Enter date (dd/mm/yyyy): ")
-        add_transactions("expense", amount, date, "Transport")
-    elif expense_choice == "4":
-        amount = float(input("Enter amount: "))
-        date = input("Enter date (dd/mm/yyyy): ")
-        add_transactions("expense", amount, date, "Entertainment")
-    elif expense_choice == "5":
-        amount = float(input("Enter amount: "))
-        date = input("Enter date (dd/mm/yyyy): ")
-        add_transactions("expense", amount, date, "Other")
-    elif expense_choice == "6":
-        amount = float(input("Enter amount: "))
-        date = input("Enter date (dd/mm/yyyy): ")
-        add_transactions("expense", amount, date, "Unexpected")
-
-    else:
-        print("Invalid expense option.")
-
-        if not transactions:
-            print("No transactions yet.")
-        else:
-            for item in transactions:
-                print(f"Date: {item['date']} | {item['category']} ({item['type']}) | ${item['amount']}")
-
-##
-
-# balance
+            elif income_choice == "4":
+                print("Go to main menu")
+        break
