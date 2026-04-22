d387a69603e05d22554f20214779872825eaacbd
Anna Buczak <ab25014@essex.ac.uk>
2026-04-22 12:05:23 +0100

    final version

---
+++ fundamentals-of-programming/The Library of Forgotten Man.py
@@ -0,0 +1,25 @@
+import random
+import time
+import json
+
+room_list = []
+inventory = []
+items = []
+
+
+def the_sanctuary():
+    for items in the_sanctuary:
+        print("Portal")
+        print("The Riddle")
+        print("Secret Box")
+
+
+def print_room_list(room_list):
+    for room in room_list:
+        print("1.The Sanctuary")
+        print("2.Safe Heaven")
+        print("3.The Cursed Estate ")
+        print("4.House of Eccentrics")
+        print("5.The Archive of Unwritten Things")
+        print("6.The Archive of Unwritten Things")
+        print("6.Door with thousands locks")
