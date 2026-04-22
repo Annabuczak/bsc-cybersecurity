2d887cd72fc764a7549bb1d00707b6215c36191c
Anna Buczak <ab25014@essex.ac.uk>
2026-04-22 17:28:18 +0100

    final version

--- fundamentals-of-programming/The Library of Forgotten Man.py
+++ fundamentals-of-programming/The Library of Forgotten Man.py
@@ -75,9 +75,10 @@ class Inventory:
 
 
 def print_intro():
+    current_room = "The Sanctuary"
     print(
         "The air smelled of dust and forgotten stories. Somewhere deep inside the labyrinth, a book was waiting…for you")
-    print("Sebastian, wake up, from your dream...or reality")
+    print("Sebastian, wake up from your dream...or reality")
     print("Welcome to the Library of Forgotten Man")
     print(
         "You find yourself in a place that feels both familiar and strange. The air is thick with a sense of nostalgia and mystery")
@@ -85,4 +86,16 @@ def print_intro():
         "You are in The Sanctuary, a vast, silent hall where shadow cling to towering shelves filled with books that seem to whisper secrets of the past.")
     print("The scent of old paper and leather fills the air, and the dim light casts eerie shadows on the walls.")
     print(
-        "This is where your journey begins, Sebastian. Here, you will uncover the mysteries of the past and confront the forgotten truths about Julian Carax, the broken writer who soul desires only one thing...revenge .")
+        "This is where your journey begins, Sebastian. Here, you will uncover the mysteries of the past and confront the forgotten truths about Julian Carax, the broken writer whose soul desires only one thing...revenge.")
+    print(
+        "Sebastian, to uncover the truth you need to solve the riddle of the Sanctuary.")
+    print("Find five items.")
+    print("Each item represents identity, love, legacy, creation and tragedy.")
+    print("Combined they unlock the box and reveal the Golden Key.")
+    print("The Golden Key? Well, don't waste precious time because...")
+    print("...it is time to...")
+    print("...play the game, Sebastian!")
+    print("...")
+
+
+input("Press enter to begin.")
