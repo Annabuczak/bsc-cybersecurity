b0ef3afac822aed44e885829a290fbc09d07c165
Anna Buczak <ab25014@essex.ac.uk>
2026-04-24 14:01:56 +0100

    final version

--- fundamentals-of-programming/The Library of Forgotten Man.py
+++ fundamentals-of-programming/The Library of Forgotten Man.py
@@ -57,7 +57,19 @@ time.sleep(2)
 import json
 
 items = ["Portal", "The Riddle", "Secret Box"]
+clockwise_order = [
 
+    "Safe Heaven",
+
+    "The Cursed Estate",
+
+    "House of Eccentrics",
+
+    "The Archive of Unwritten Things",
+
+    "The Place of Torment"
+
+]
 rooms = {
     "The Sanctuary": {
         "north": "Safe Heaven",
@@ -283,7 +295,7 @@ def play_again():
             print_sleep("Thanks for playing! See you next time.")
             exit()
         elif again == 'yes':
-            main()
+            menu()
         else:
             print("Invalid input. Please enter 'yes' or 'no'.")
 
@@ -318,9 +330,48 @@ def movment():
         print(f"Moving to direction: {x}, {y}")
 
 
-# ROOMS
-def santuary():
-    print("You are in the Sanctuary")
+# MOVEMENT AROUND ROOMS
+current_room = "The Sanctuary"
+
+
+def move_player(current_room, rooms, clockwise_order):
+    print(f"\nYou are in {current_room}")
+    for direction in rooms[current_room]:
+        print(f"You can go {direction} to {rooms[current_room][direction]}")
+
+    move = input("> ").strip().lower()
+    if move not in rooms[current_room]:
+        print("You can't go that way.")
+        return current_room
+
+    next_room = rooms[current_room][move]
+
+    if current_room == "The Sanctuary":
+        if next_room == "Safe Heaven":
+            print("You step forward... the journey begins.")
+            return next_room
+        else:
+            print("Something feels wrong... this is not the right path.")
+            return current_room
+
+    if current_room in clockwise_order:
+        current_index = clockwise_order.index(current_room)
+
+        if current_index + 1 < len(clockwise_order):
+            correct_next = clockwise_order[current_index + 1]
+
+            if next_room == correct_next:
+                print("You move forward...")
+                return next_room
+            else:
+                print("You feel lost... this is not the correct path.")
+                return current_room
+
+    return current_room
+
+
+def safe_heaven():
+    print("You are in Safe Heaven")
 
 
 def the_cursed_estate():
