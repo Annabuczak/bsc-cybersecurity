f7dde2e7214a322c9bc36e76a59b6bef5b381594
Anna Buczak <ab25014@essex.ac.uk>
2026-04-27 12:51:46 +0100

    intro_riddle.py

--- fundamentals-of-programming/the_library_game/main.py
+++ fundamentals-of-programming/the_library_game/main.py
@@ -28,19 +28,21 @@ print("Total words:", total_words)
 # MOVEMENT FUNCTION - DONE
 # MENU FUNCTION - DONE
 # ROOM FUNCTIONS - in progress
-# SAVE/LOAD -DONE
 # take FUNCTION - Done
 # drop ITEM FUNCTION - TO DO
 # use ITEM FUNCTIONS - TO DO
 
 # DEF ORDER ( TO BE SORTED)
 from save_load import save_game, load_game
+
+data = load_game()
+
+if data:
+    current_room = data["current_room"]
+    inventory = data["inventory"]
 import time
 
 time.sleep(2)
-
-import json
-import os
 
 portal_items = []
 items_needed = ["Letter", "Photo", "Pen", "Book", "Newspaper"]
@@ -99,61 +101,6 @@ rooms = {
         "item": None
     }
 }
-
-
-def print_intro():
-    current_room = "The Sanctuary"
-    print(
-        "The air smelled of dust and forgotten stories. Somewhere deep inside the labyrinth, a book was waiting…for you...")
-    print("Sebastian, wake up from your dream...or reality")
-    print("...")
-    print("Welcome to the Library of Forgotten Man")
-    print(
-        "You find yourself in a place that feels both familiar and strange. The air is thick with a sense of nostalgia and mystery")
-    print(
-        "You are in The Sanctuary, a vast, silent hall where shadow cling to towering shelves filled with books that seem to whisper secrets of the past.")
-    print("The scent of old paper and leather fills the air, and the dim light casts eerie shadows on the walls.")
-    print(
-        "This is where your journey begins, Sebastian. Here, you will uncover the mysteries of the past and confront the forgotten truths about Julian Carax, the broken writer whose soul desires only one thing...revenge.")
-    print(
-        "Sebastian, to uncover the truth you need to solve the riddle of the Sanctuary.")
-    print("Find five items.")
-    print("Each item represents identity, love, legacy, creation and tragedy.")
-    print("Combined they unlock the box and reveal the Golden Key.")
-    print("The Golden Key? Well, don't waste precious time because...")
-    print("...it is time to...")
-    print("...play the game, Sebastian!")
-    print("Good luck and God bless")
-    print("...")
-
-
-def the_riddle():
-    current_room = "The Sanctuary"
-    print("Solve the riddle to find the truth...")
-    print("...")
-    print("Five fragments lie where shadows keep,")
-    print("In silent ink and memories deep.")
-    print("...")
-    print("Begin where silence shelters truth")
-    print(" Where quiet souls are laid to rest")
-    print("...")
-    print("A name concealed in crimson thread,")
-    print("A love remembered, through half is dead.")
-    print("...")
-    print("Seek next the halls where sorrow stays,")
-    print("A house still bound to darker days.")
-    print("...")
-    print("Then walk where echoes twist the mind")
-    print("Where curious thoughts grow unconfines")
-    print("Where histories are written in sacred black blood")
-    print("...")
-    print("Beyond, where words were never penned,")
-    print("The truths unfinished start to bend.")
-    print("...")
-    print("A story lost, yet still your own,")
-    print("A truth consumed by ash and flame")
-    print("...")
-    print("Restore the past to reclaim the name")
 
 
 def menu():
