aba7b09620b2470f9e6d9b99db421572544d1b3a
Anna Buczak <ab25014@essex.ac.uk>
2026-06-04 16:17:55 +0100

    main.py

---
+++ object-oriented-programming/polymorphism.py
@@ -0,0 +1,36 @@
+class Employee:
+    company = "THM"
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def show_details(self):
+        print(f"{self.name} earns {self.salary} per year.")
+
+
+class Driver(Employee):
+    def __init__(self, name, salary, vehicle_type):
+        super().__init__(name, salary)
+        self.vehicle_type = vehicle_type
+
+    def show_details(self):
+        print(f"{self.name} earns {self.salary} per year and drives a {self.vehicle_type}.")
+
+
+class Developer(Employee):
+    def __init__(self, name, salary, programming_language):
+        super().__init__(name, salary)
+        self.programming_language = programming_language
+
+    def show_details(self):
+        print(f"{self.name} earns {self.salary} per year and codes in {self.programming_language}.")
+
+
+driver = Driver("Anna", 14500, "car")
+developer = Developer("Lou", 25000, "Python")
+
+employees = [driver, developer]
+
+for employee in employees:
+    employee.show_details()
