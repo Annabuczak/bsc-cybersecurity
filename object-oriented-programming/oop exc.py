9f6776d5d46c17c7de8ccfd88d1c5e61aaaad692
Anna Buczak <ab25014@essex.ac.uk>
2026-06-15 19:04:05 +0100

    main.py

--- object-oriented-programming/oop exc.py
+++ object-oriented-programming/oop exc.py
@@ -7,14 +7,15 @@ class BankAccount:
 
     def account_users(self):
         print(
-            f"Account holder {self.owner}, has  {self.balance}, after paying in {self.deposit},and taking out  {self.withdrawal}")
+            f"Account holder {self.owner}, has {self.balance}, after paying in {self.deposit}, and taking out {self.withdrawal}"
+        )
         return self.balance + self.deposit - self.withdrawal
 
     def calculate_balance(self):
         return self.balance + self.deposit - self.withdrawal
 
-    def withdraw(self, balance):
-        if balance > 100:
+    def withdraw(self, amount):
+        if amount > self.calculate_balance():
             print("You don't have enough money!")
         else:
             print("You have enough money!")
@@ -22,12 +23,12 @@ class BankAccount:
 
 account_user1 = BankAccount("John", 20000, 10000, 110)
 account_user2 = BankAccount("Anna", 40000, 1003, 90)
+
 account_user1.account_users()
 account_user2.account_users()
+
 account_user1.withdraw(110)
-account_user2.withdraw(9)
-account_user1.account_users()
+account_user2.withdraw(90)
+
 print(account_user1.account_users())
 print(account_user2.account_users())
-print(account_user1.withdraw(110))
-print(account_user2.withdraw(90))
