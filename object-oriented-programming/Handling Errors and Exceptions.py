2e084d72c979cdc7c2aab9f861bf952d3cac5ecd
Anna Buczak <ab25014@essex.ac.uk>
2026-06-23 10:32:53 +0100

    main.py

--- object-oriented-programming/Handling Errors and Exceptions.py
+++ object-oriented-programming/Handling Errors and Exceptions.py
@@ -13,3 +13,30 @@ except ValueError:
 
 except ZeroDivisionError:
     print("Number of people cannot be 0.")
+
+
+# Student Marks File Reader
+def read_marks(filename):
+    try:
+        with open(filename, "r") as file:
+            marks = []
+
+            for line in file:
+                mark = int(line.strip())  # May raise ValueError
+                marks.append(mark)
+
+            return marks
+
+    except FileNotFoundError:
+        raise
+
+
+try:
+    marks = read_marks("marks.txt")
+    print("Marks:", marks)
+
+except FileNotFoundError:
+    print("marks.txt not found.")
+
+except ValueError:
+    print("One of the marks in the file is invalid.")
